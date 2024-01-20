85b2c34f104cdc85ce7c12bd6fd0eee60525339b
bekzodnj <bekzodnx@gmail.com>
2024-01-20 16:19:49 +0100

    Navigation: pending, loading changes

--- app/root.tsx
+++ app/root.tsx
@@ -1,4 +1,4 @@
-import { json } from "@remix-run/node";
+import { json, redirect } from "@remix-run/node";
 
 import {
   Form,
@@ -8,6 +8,8 @@ import {
   Meta,
   Outlet,
   Scripts,
+  NavLink,
+  useNavigation,
   useLoaderData,
   ScrollRestoration,
 } from "@remix-run/react";
@@ -27,11 +29,13 @@ export const loader = async () => {
 
 export const action = async () => {
   const contact = await createEmptyContact();
-  return json({ contact });
+
+  return redirect(`/contacts/${contact.id}/edit`);
 };
 
 export default function App() {
   const { contacts } = useLoaderData<typeof loader>();
+  const navigation = useNavigation();
   return (
     <html lang="en">
       <head>
@@ -63,7 +67,12 @@ export default function App() {
               <ul>
                 {contacts.map((contact) => (
                   <li key={contact.id}>
-                    <Link to={`contacts/${contact.id}`}>
+                    <NavLink
+                      className={({ isActive, isPending }) =>
+                        isActive ? "active" : isPending ? "pending" : ""
+                      }
+                      to={`contacts/${contact.id}`}
+                    >
                       {contact.first || contact.last ? (
                         <>
                           {contact.first} {contact.last}
@@ -72,7 +81,7 @@ export default function App() {
                         <i>No Name</i>
                       )}{" "}
                       {contact.favorite ? <span>★</span> : null}
-                    </Link>
+                    </NavLink>
                   </li>
                 ))}
               </ul>
@@ -83,7 +92,10 @@ export default function App() {
             )}
           </nav>
         </div>
-        <div id="detail">
+        <div
+          className={navigation.state === "loading" ? "loading" : ""}
+          id="detail"
+        >
           <Outlet />
         </div>
 
